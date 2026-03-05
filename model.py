7029d060793f6af277d7ec229940df7cd67c4c89
mark anthony dador <dador.markanthony@gmail.com>
2026-03-05 21:07:07 +0800

    add model.py,requirements.txt

---
+++ model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class Products(BaseModel):
+    id:int
+    sales:str
+    items:str
+    price:int
+    quantity:int
+
+class Users(BaseModel):
+    user_id:int
+    username:str
+    password:str
